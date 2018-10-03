4c7f3c82051bdaab45868020cda0c042f8093a9a
David Velez <dveleztx@gmail.com>
2018-10-03 01:31:45 -0500

    Framework Created for Challenge 1

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+public abstract class Player {
+
+    private String name;
+    private String profession;
+    private String race;
+    private int hitPoints;
+    private int armorClass;
+    private String weapon;
+    private int level;
+
+    public Player (String name, String profession, String race) {}
+
+    public void attack() {}
+
+    public void search() {}
+}
